2e6946bb4c1240b8a33f941d298053d230deb809
ckiick1234 <ckiick1234@gmail.com>
2025-04-01 20:33:00 -0700

    update

--- obj/Release/net9.0/api.AssemblyInfo.cs
+++ obj/Release/net9.0/api.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86c989a13ea0da31948d1f0c0d0d82250f709bf9")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
